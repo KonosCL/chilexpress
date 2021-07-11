ee33c9efc55181fb432a8db2566ee0fa2391ac67
Daniel Blanco <daniel@blancomartin.cl>
2021-07-11 10:10:49 -0300

    [FIX] delivery_cl_express: rearrange manifest file. Change license to LGPL

--- delivery_cl_express/__manifest__.py
+++ delivery_cl_express/__manifest__.py
@@ -1,16 +1,28 @@
-# -*- coding: utf-8 -*-
-# 
 {
-    'name': "Chile Express Delivery",
-    'description': "Calcular costo de envío con Chile Express API",
-    'author': 'Federico Nardi',
-    'category': 'Warehouse',
+    'name': "Chilexpress Delivery",
     'version': '1.0',
+    'category': 'Warehouse',
+    'sequence': 12,
+    'author': 'Federico Nardi',
+    'description': """
+Chilexpress Connector
+=====================
+Calculate the delivery cost using Chilexpress API
+""",
+    'website': '',
     'depends': ['delivery', 'mail'],
+    'external_dependencies': {
+        'python': [
+        ],
+    },
     'data': [
         'views/delivery_cl_express_view.xml',
         #'views/res_config_settings_views.xml',
         'data/cl_exp_code.xml',
     ],
-    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+    'license': 'LGPL-3',
 }
+
